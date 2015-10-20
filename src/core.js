18e90c903e68ed35a47c71b30234b052558af993
Trexnix <trexnix@gmail.com>
2015-10-20 14:11:51 +0700

    Refactor core.js

--- src/core.js
+++ src/core.js
@@ -4,11 +4,8 @@ var YouTubeLoopButton = (function(d) {
   var videoID;
 
   var getVideoID = function() {
-    var params = document.location.search.substr(1).split('&');
-    params = params.filter(function(v) { return v.indexOf('v=') > -1 });
-
-    if (params.length)
-      return params[0].split('=')[1];
+    var match = document.location.search.match(/v=([^&]+)/);
+    return (match !== null) ? match[1] : match;
   };
 
   // Run everytime user navigates on the page
@@ -34,12 +31,8 @@ var YouTubeLoopButton = (function(d) {
       loopButton.innerHTML = '<svg width=100% height=100% viewBox="0 0 36 36" version=1.1 xmlns=http://www.w3.org/2000/svg xmlns:xlink=http://www.w3.org/1999/xlink><defs><path id=ytp-1000 d="M 8 11 L 24 11 L 24 9 L 28 12.5 L 24 16 L 24 14 L 11 14 L 11 18 L 8 18 Z"></path><path id=ytp-1001 d="M 25 18 L 28 18 L 28 25 L 12 25 L 12 27 L 8 23.5 L 12 20 L 12 22 L 25 22 Z"></path></defs><use xlink:href=#ytp-1000 class=ytp-svg-fill></use><use xlink:href=#ytp-1001 class=ytp-svg-fill></use></svg>';
 
       loopButton.addEventListener('click', function(e) {
-        videoElement.loop = videoElement.loop ? false : true;
-
-        if (videoElement.loop)
-          history.add(videoID);
-        else
-          history.remove(videoID);
+        videoElement.loop = !videoElement.loop;
+        videoElement.loop ? history.add(videoID) : history.remove(videoID);
 
         // Update the button's state
         this.classList.toggle('ytp-button-toggled', videoElement.loop);
